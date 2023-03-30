df2930435c94633710589274264a9fb48ef6836e
권민수 <kd0615@naver.com>
2023-03-30 10:26:42 +0900

    이미지뷰앱 실습

--- ImageView_03/ViewController.swift
+++ ImageView_03/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
+    //초기화면 실행
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.png")
+        imgOff = UIImage(named: "lamp_off.png")
+        
+        imgView.image = imgOn
+        
     }
 
 
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        //상수선언: 확대시킬 값
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if (isZoom) {
+            newWidth = imgView.frame.width / scale
+            newHeight = imgView.frame.height / scale
+            
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {
+            newWidth = imgView.frame.width * scale
+            newHeight = imgView.frame.height * scale
+            
+            btnResize.setTitle("축소", for: .normal)
+        }
+        
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
+    
 }
 
